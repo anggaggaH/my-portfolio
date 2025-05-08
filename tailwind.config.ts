cf0579e7daa1a63aacac1dc45111197f5e342107
angga <anggah.net@gmail.com>
2025-05-08 16:40:58 +0700

    update: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
 	content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: { extend: {} },
 	plugins: [],
+	future: {
+		// Tailwind doesn't officially expose this, but it works
+		unstable_disableOpacityUtilitiesByDefault: false,
+		unstable_disablePreflightByDefault: false,
+		// this is the one that matters
+		disableExperimentalPlugins: true,
+	},
 };
 
 export default config;
