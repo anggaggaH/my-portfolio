d787a44702029a665122845d364567347cf40dfa
angga <anggah.net@gmail.com>
2025-05-15 17:07:51 +0700

    update: page.server query

--- src/app/projects/[slug]/page.server.tsx
+++ src/app/projects/[slug]/page.server.tsx
@@ -1,9 +1,9 @@
-import client from '@/lib/sanity';
+import { client } from '@/lib/sanity';
 import { Metadata } from 'next';
 
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-	const query = encodeURIComponent(`*[_type == "project" && slug.current == "${params.slug}"][0]{title, overview}`);
-	const res = await client.get(`?query=${query}`);
+	const query = `*[_type == "project" && slug.current == "${params.slug}"][0]{title, overview}`;
+	const res = await client.fetch(query);
 	const project = res.data.result;
 
 	if (!project) {
