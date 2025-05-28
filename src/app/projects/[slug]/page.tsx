e7ce1241c3735187c474675e067e231a43a9316b
angga <anggah.net@gmail.com>
2025-05-28 10:23:24 +0700

    Adapts project page to handle async params
    Updates the project page to properly handle the asynchronous nature of the slug parameter.

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,8 +2,9 @@ import { notFound } from 'next/navigation';
 import ProjectDetailClientPage from './page.client';
 import { getProject } from '@/hooks/sanity/projectQuery';
 
-export default async function Page({ params }: { params: { slug: string } }) {
-	const project = await getProject({ slug: params.slug });
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+	const { slug } = await params
+	const project = await getProject({ slug: slug });
 
 	if (!project) {
 		notFound();
