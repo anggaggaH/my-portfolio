4d028fce88db21f312245868800f4c864d9b65e7
angga <anggah.net@gmail.com>
2025-05-28 09:20:02 +0700

    Refines project detail page props

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,7 +2,13 @@
 import ProjectDetailClientPage from './page.client';
 import { getProject } from '@/hooks/sanity/projectQuery';
 
-export default async function ProjectDetailPage({ params }: { params: { slug: any } }) {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function ProjectDetailPage({ params }: PageProps) {
 	const project: any = await getProject({
 		slug: params.slug,
 	});
