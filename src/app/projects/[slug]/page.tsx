a13aafe7abc054c946ddc9d9689697cb78913e68
angga <anggah.net@gmail.com>
2025-05-28 10:05:18 +0700

    update

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,12 +2,12 @@ import { notFound } from 'next/navigation';
 import ProjectDetailClientPage from './page.client';
 import { getProject } from '@/hooks/sanity/projectQuery';
 
-export default async function ProjectDetailPage({ params }: { params: { slug: string } }) {
-	const project = await getProject({
-		slug: params.slug,
-	});
+export default async function Page({ params }: { params: { slug: string } }) {
+	const project = await getProject({ slug: params.slug });
+
 	if (!project) {
 		notFound();
 	}
+
 	return <ProjectDetailClientPage project={project} />;
 }
