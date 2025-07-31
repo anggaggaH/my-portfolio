80a118f66dda380e819e999b82df101c0c24cf6d
angga <anggah.net@gmail.com>
2025-07-31 16:44:18 +0700

    feat: enhance SkillsSection with additional icons and skills

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { SiReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiVite, SiGithub, SiGatsby, SiElement, SiGitlab } from 'react-icons/si';
+import { SiReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiVite, SiGithub, SiGatsby, SiElement, SiGitlab, SiMaterialdesign, SiSass } from 'react-icons/si';
 import { DiVisualstudio  } from 'react-icons/di';
 import { FaBootstrap, FaPhp, FaVuejs } from 'react-icons/fa';
-import { BsFillChatDotsFill } from "react-icons/bs";
+import { BsCursorFill, BsFillChatDotsFill } from "react-icons/bs";
 
 const skills = {
 	Frontend: [
@@ -18,7 +18,9 @@ const skills = {
 	Styling: [
 		{ name: 'TailwindCSS', icon: SiTailwindcss },
 		{ name: 'Bootstrap', icon: FaBootstrap },
-		{ name: 'Element Plus', icon: SiElement }
+		{ name: 'Element Plus', icon: SiElement },
+		{ name: 'Material UI', icon: SiMaterialdesign },
+		{ name: 'Sass', icon: SiSass },
 	],
 	'Backend / BaaS': [
 		// { name: 'Firebase', icon: SiFirebase },
@@ -26,9 +28,10 @@ const skills = {
 	],
 	Tools: [
 		{ name: 'VS Code', icon: DiVisualstudio },
+		{ name: 'ChatGPT', icon: BsFillChatDotsFill },
+		{ name: 'Cursor', icon: BsCursorFill },
 		{ name: 'GitLab', icon: SiGitlab },
 		{ name: 'GitHub', icon: SiGithub },
-		{ name: 'ChatGPT', icon: BsFillChatDotsFill },
 	],
 };
 
